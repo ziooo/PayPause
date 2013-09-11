47a523922175fe70a8f19229a32b07f3c22eb49a
Kévin Pogorzelski <kevin.pogorzelski@gmail.com>
2013-09-11 23:10:46 +0200

    Ajout des Activités : Main, AddProfil, Rapide. Les deux dernières activités sont vides.

---
+++ PayPause/src/main/java/com/ziooo/paypause/Rapide.java
@@ -0,0 +1,22 @@
+package com.ziooo.paypause;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+/**
+ * Created by kevin on 11/09/13.
+ */
+public class Rapide extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rapide);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
