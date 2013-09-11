f99ebe664a153efaf2290def16b06e5226db4a8a
Kévin Pogorzelski <kevin.pogorzelski@gmail.com>
2013-09-11 23:32:54 +0200

    Revert "Ajout des Activités : Main, AddProfil, Rapide. Les deux dernières activités sont vides."
    This reverts commit 47a523922175fe70a8f19229a32b07f3c22eb49a.

--- PayPause/src/main/java/com/ziooo/paypause/MainActivity.java
+++ PayPause/src/main/java/com/ziooo/paypause/MainActivity.java
@@ -1,40 +1,15 @@
 package com.ziooo.paypause;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    private TextView addProfil;
-    private Button rapide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        addProfil = (TextView) findViewById(R.id.addProfil);
-        addProfil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddProfil.class);
-                startActivity(intent);
-            }
-        });
-
-        rapide = (Button) findViewById(R.id.rapide);
-        rapide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Rapide.class);
-                startActivity(intent);
-            }
-        });
     }
 
 
@@ -44,4 +19,5 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
 }
